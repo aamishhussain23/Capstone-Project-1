3f63c82afff4b703a0c510c531510e276a71fb84
Aamish Hussain <aamishhussain23@gmail.com>
2023-10-18 23:44:09 +0530

    added registration page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import appCss from './styles/App.module.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Registration from './components/Registration';
+import Error from './components/Error';
+import Home from './components/Home';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/register' element={<Registration></Registration>}></Route>
+        <Route path='*' element={<Error></Error>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
